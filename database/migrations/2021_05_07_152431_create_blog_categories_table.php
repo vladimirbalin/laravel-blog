83725980dac341fb505b66d13c20a2704fd48a62
vladimirB <vladimirzq@gmail.com>
2021-05-09 20:37:25 +0700

    modificate columns at migrations tables

--- database/migrations/2021_05_07_152431_create_blog_categories_table.php
+++ database/migrations/2021_05_07_152431_create_blog_categories_table.php
@@ -14,8 +14,14 @@ class CreateBlogCategoriesTable extends Migration
     public function up()
     {
         Schema::create('blog_categories', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->integer('parent_id')->unsigned()->default(0);
+            $table->string('slug')->unique();
+            $table->string('title');
+            $table->text('description')->nullable();
+
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
