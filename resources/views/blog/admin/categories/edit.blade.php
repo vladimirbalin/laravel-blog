12e4f089c408eaa3f69cdf7a99d1971cfb089778
vladimirB <vladimirzq@gmail.com>
2021-05-12 05:33:09 +0700

    fix views - admin edit categories page

--- resources/views/blog/admin/categories/edit.blade.php
+++ resources/views/blog/admin/categories/edit.blade.php
@@ -5,7 +5,9 @@
         @php /** @var \Illuminate\Support\ViewErrorBag $errors */ @endphp
         @if($errors->any())
             <div class="alert alert-danger alert-dismissible fade show my-4" role="alert">
-                {{ $errors->first() }}
+                @foreach($errors->all() as $error)
+                    <span>{{$error}}</span><br>
+                @endforeach
                 <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -27,7 +29,7 @@
                 <div class="col-md-8 py-3">
                     @include('blog.admin.categories.includes.left-part')
                 </div>
-                <div class="col-md-3 py-3">
+                <div class="col-md-4 py-3">
                     @include('blog.admin.categories.includes.right-part')
                 </div>
 
