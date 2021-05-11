4859e15f989acbba3138db7eba112f2d947d543a
vladimirB <vladimirzq@gmail.com>
2021-05-12 05:08:57 +0700

    add PHPDoc for models

--- app/Models/BlogCategory.php
+++ app/Models/BlogCategory.php
@@ -4,8 +4,40 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * App\Models\BlogCategory
+ *
+ * @property int $id
+ * @property int $parent_id
+ * @property string $slug
+ * @property string $title
+ * @property string|null $description
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string|null $deleted_at
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogCategory newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogCategory newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogCategory query()
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogCategory whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogCategory whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogCategory whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogCategory whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogCategory whereParentId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogCategory whereSlug($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogCategory whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogCategory whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class BlogCategory extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'parent_id',
+        'slug',
+        'description'
+    ];
 }
--- app/Models/BlogPost.php
+++ app/Models/BlogPost.php
@@ -6,6 +6,44 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * App\Models\BlogPost
+ *
+ * @property int $id
+ * @property int $category_id
+ * @property int $user_id
+ * @property string $slug
+ * @property string $title
+ * @property string|null $excerpt
+ * @property string $content_raw
+ * @property string $content_html
+ * @property int $is_published
+ * @property string|null $published_at
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property \Illuminate\Support\Carbon|null $deleted_at
+ * @method static \Database\Factories\BlogPostFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost newQuery()
+ * @method static \Illuminate\Database\Query\Builder|BlogPost onlyTrashed()
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost query()
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost whereCategoryId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost whereContentHtml($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost whereContentRaw($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost whereExcerpt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost whereIsPublished($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost wherePublishedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost whereSlug($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BlogPost whereUserId($value)
+ * @method static \Illuminate\Database\Query\Builder|BlogPost withTrashed()
+ * @method static \Illuminate\Database\Query\Builder|BlogPost withoutTrashed()
+ * @mixin \Eloquent
+ */
 class BlogPost extends Model
 {
     use HasFactory, SoftDeletes;
