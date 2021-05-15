10a0514a0fe5e5471a0a5cac3e74bea479e94177
vladimirB <vladimirzq@gmail.com>
2021-05-15 22:41:06 +0700

    moved Auth controllers to Blog/Admin/Auth; remove 'confirm', 'forgot password', 'reset password', 'verification' controllers; refactor app layout nav and routers

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\Blog\Admin\Auth\LoginController;
+use App\Http\Controllers\Blog\Admin\Auth\RegisterController;
 use App\Http\Controllers\Blog\Admin\CategoryController;
+use App\Http\Controllers\Blog\Admin\PostController as AdminPostController;
 use App\Http\Controllers\Blog\PostController;
 use Illuminate\Support\Facades\Route;
 
@@ -20,12 +23,16 @@ Route::group(['prefix' => 'blog'], function () {
         ->names('blog.posts');
 });
 Route::group(['prefix' => 'admin/blog'], function () {
+    Route::get('/login', [LoginController::class, 'showLoginForm'])->name('admin.login');
+    Route::post('/login', [LoginController::class, 'login'])->name('admin.login-post');
+    Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('admin.register');
+    Route::post('/register', [RegisterController::class, 'register'])->name('admin.register-post');
+    Route::post('/logout', [LoginController::class, 'logout'])->name('admin.logout');
+
     $methods = ['index', 'create', 'store', 'edit', 'update'];
     Route::resource('categories', CategoryController::class)
         ->only($methods)
         ->names('blog.admin.categories');
 });
-
-Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
